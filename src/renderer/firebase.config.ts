60f5e11ccd9e54b777caa54ccfc38a2caaa1c0c4
Ryan Sheehan <ryansheehan@users.noreply.github.com>
2017-07-08 14:39:52 -0500

    removed outdated info in firebase config

--- src/renderer/firebase.config.ts
+++ src/renderer/firebase.config.ts
@@ -3,12 +3,12 @@ import * as firebase from 'firebase/app';
 
 // App initialization
 const firebaseApp: firebase.app.App = firebase.initializeApp({
-  apiKey: "AIzaSyAo6xJ0zsKWkrIzPOET0KfEiFrVD7SkD24",
-  authDomain: "picjade-11cec.firebaseapp.com",
-  databaseURL: "https://picjade-11cec.firebaseio.com",
+  apiKey: "<api_key>",
+  authDomain: "<domain>.firebaseapp.com",
+  databaseURL: "https://<db_url>.firebaseio.com",
   storageBucket: "",
-  messagingSenderId: "93538683713"
-}, "emotivent");
+  messagingSenderId: "<sender_id>"
+}, "vue-ts-electron-seed");
 
 
 export default firebaseApp;
